4599699cab132a1dbfa98981fa84aa1b54a1e5af
Hamsterkiller <reven_yas_reven@mail.ru>
2018-06-07 08:39:29 +0300

    Course proceedings
    Added code of changing state of the App.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,23 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // DO NOT DO THIS: this.state.persons[0].name = 'Maximilian';
+    // Direct mutation if the state not allowed!
+    this.setState({
+    persons: [
+      {name: "Maximilian", age: "31"},
+      {name: "Helen", age: "19"},
+      {name: "Mark", age: "28"}
+    ]
+   })
+  }
+
   render() {
+    // use switchNameHandler without () to prevent it to be called immediately after the browser renders the page
+    // events, supported by ReactJS:
+    // https://reactjs.org/docs/events.html#supported-events
     return (
       <div className="App">
         <header className="App-header">
@@ -23,7 +39,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> And I have a little mouse. </Person>
+        <button onClick={this.switchNameHandler}>Click!</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> </Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> </Person>
       </div> 
       // return React.createElement('div', null, React.createElement('h1', null, 'Does this work now?'))
