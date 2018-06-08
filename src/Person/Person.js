82c2ce859e222e4499b3342dd005a1318d1ba50f
Hamsterkiller <reven_yas_reven@mail.ru>
2018-06-08 17:06:07 +0300

    Commit!

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 // in the simplest form, a component is just a function, that returns JSX
 const person = (props) => {
+	/* we can pass methods as props, like it's done in <p onClick={props.click}> */
 	return (
 		<div>
-			<p> I'm a {props.name} and I am {props.age} years old. </p>
+			<p onClick = {props.click}> I'm a {props.name} and I am {props.age} years old. </p>
 			<p>{props.children}</p>
 		</div>
 		)
